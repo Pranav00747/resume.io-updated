fd0dbe6c15db9de4fdcdd7d3a4a55d72fc2908a1
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2023-02-14 04:48:31 +0530

    Create Special-Something.r

---
+++ Special-Something.r
@@ -0,0 +1,12 @@
+#Something short
+
+Me <- "Pranav"
+
+Todays_Special <-function()
+{
+   if(Me)
+    {
+        Something <- "I am not in mood because i lost my card i have uploaded because the day is special and how can I upload this low thinking currently i have created whole ML Product, Mail API, Chat AI and soon i will leave."
+        Special <- "Thank you !"
+    }
+}
