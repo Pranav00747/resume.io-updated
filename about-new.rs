9c2c8528d8921d2ec276b8f10237d7f0e35ee2d2
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2023-03-16 02:22:45 +0530

    Create about-new.rs

---
+++ about-new.rs
@@ -0,0 +1,12 @@
+mod resume_io_update_securing_personal
+{
+fn to_open_this_app_and_new(its_my_life : string)-> string
+{
+   if(its_my_life=="It's off limits right now")
+{
+   let do_this : string ="All pic files jpgs jpges will their with app.";
+   show_comment(do_this);
+   return "Soon i will launch Mail API and Deep Learning Kit machine learning tool and also Chat GPT AI i have created that's  the other thing. Thanks & Keep Support !";
+}
+}
+}
